e5fb657bf74c77d604781a31f858d16eba7be933
pakhuta.siarhei <pakhuta.siarhei@gmail.com>
2017-11-13 01:05:24 +0300

    task5 -> fixed comments from task 4

--- src/services/storage/index.js
+++ src/services/storage/index.js
@@ -10,15 +10,21 @@ const sources = [
     { name: 'users', Model: User, hasFile: true },
     { name: 'token', Model: null, hasFile: false }
 ];
-let storageInstance;
+let volumes;
 
-class StorageInstance {
-    constructor() {
+export default class Storage {
+    static init() {
+        if (volumes) {
+            return;
+        }
+
+        volumes = {};
+
         sources.forEach(source => {
             if (source.hasFile) {
                 try {
                     let data = fs.readFileSync(path.join(__dirname, '../..', config.dataPath, `${source.name}.json`));
-                    this.add(source, JSON.parse(data.toString()));
+                    Storage.add(source.name, JSON.parse(data.toString()));
                 } catch (err) {
                     Output.write(err);
                 }
@@ -26,40 +32,10 @@ class StorageInstance {
         });
     }
 
-    add(source, data) {
-        let insertedData = [];
-        let isDataArray = Array.isArray(data);
-
-        try {
-            if (!this[source.name]) {
-                this[source.name] = [];
-            }
-
-            if (!isDataArray) {
-                data = [data];
-            }
-
-            insertedData = data.map(dataItem => (source.Model) ? (new source.Model(dataItem)) : (dataItem));
-            this[source.name] = this[source.name].concat(insertedData);
-        } catch (err) {
-            Output.write(err);
-        }
-
-        return (isDataArray) ? (insertedData) : (insertedData[0]);
-    }
-
-    static getSourceParams(sourceName) {
-        return sources.find(source => source.name === sourceName);
-    }
-}
-
-export default class Storage {
     static get(sourceName, filters = []) {
-        if (!storageInstance) {
-            storageInstance = new StorageInstance();
-        }
+        Storage.init();
 
-        let data = storageInstance[sourceName];
+        let data = volumes[sourceName];
 
         filters.forEach(filter => {
             if (filter.name) {
@@ -73,10 +49,31 @@ export default class Storage {
     }
 
     static add(sourceName, data) {
-        if (!storageInstance) {
-            storageInstance = new StorageInstance();
+        Storage.init();
+
+        let source = Storage.getSourceParams(sourceName);
+        let insertedData = [];
+        let isDataArray = Array.isArray(data);
+
+        try {
+            if (!volumes[source.name]) {
+                volumes[source.name] = [];
+            }
+
+            if (!isDataArray) {
+                data = [data];
+            }
+
+            insertedData = data.map(dataItem => (source.Model) ? (new source.Model(dataItem)) : (dataItem));
+            volumes[source.name] = volumes[source.name].concat(insertedData);
+        } catch (err) {
+            Output.write(err);
         }
 
-        return storageInstance.add(StorageInstance.getSourceParams(sourceName), data);
+        return (isDataArray) ? (insertedData) : (insertedData[0]);
+    }
+
+    static getSourceParams(sourceName) {
+        return sources.find(source => source.name === sourceName);
     }
 }
